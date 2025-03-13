82022df9a38b777b177ae6573bab0e884d32ac6d
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-03-13 19:09:03 -0300

    projeto finalizado

---
+++ projetocapitura/js/script.js
@@ -0,0 +1,15 @@
+var botoes = document.querySelectorAll(".js-botao");
+
+botoes[0].addEventListener("click", function () {
+  document.body.classList.toggle("popup--aberto");
+});
+botoes[1].addEventListener("click", function () {
+  document.body.classList.toggle("popup--aberto");
+});
+botoes[2].addEventListener("click", function () {
+  document.body.classList.toggle("popup--aberto");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.classList.remove("popup--aberto");
+});
